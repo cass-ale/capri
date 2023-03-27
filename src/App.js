b2f9f4cb3fcea07c1034ce4debe6a7b671c38736
Cass Aleria <111076541+cass-ale@users.noreply.github.com>
2023-03-27 14:57:52 -0400

    Getting folders and components set up; working remotely, setup slower than would be on main PC.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import React from "react";
+import './styles.modules.css';
+import Header from "./Components/Header"
+import Main from "./Components/Main";
+import Footer from "./Components/Footer";
+
 
 
 function App() {
   return (
-    <div className="App">
+    <div>
+    <header>
+      <Header />
+    </header>
+    <main>
+      <Main />
+    </main>
+    <footer>
+      <Footer />
+    </footer>
     </div>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/reat-fontawesome";
+import {
+    faInstagram,
+    faTwitter,
+    faTiktok,
+    faYoutube,
+    faFacebook,
+    faSoundcloud
+} from "@fortawesome/free-brands-svg-icons";
+
+function Header() {
+    return(
+        <nav className="navbar">
+        <h1>CAPRI</h1>
+        </nav>
+    )
+}
+
+export default Header;
