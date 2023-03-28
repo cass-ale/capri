ad7d1dcfb14bbd06656bbb254f328f2d6efee352
Cassian Aleria <111076541+cass-ale@users.noreply.github.com>
2023-03-27 20:06:57 -0400

    Navbar foundations and font imports. Going to find better font pairings for the page links. Onwards

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './styles.modules.css';
-import Header from "./Components/Header"
+import Header from "./Components/Header";
 import Main from "./Components/Main";
 import Footer from "./Components/Footer";
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/reat-fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
     faInstagram,
     faTwitter,
@@ -9,10 +9,75 @@ import {
     faSoundcloud
 } from "@fortawesome/free-brands-svg-icons";
 
+const socials = [
+    {
+        icon: faInstagram,
+        url: "https://www.instagram.com/capricatalogue/",
+    },
+    {
+        icon: faTwitter,
+        url: "https://twitter.com/capricatalogue",
+    },
+    {
+        icon: faTiktok,
+        url: "https://www.tiktok.com/@capricatalogue",
+    },
+    {
+        icon: faYoutube,
+        url: "https://www.youtube.com/@capricatalogue",
+    },
+    {
+        icon: faFacebook,
+        url: "https://www.facebook.com/capricatalogue",
+    },
+    {
+        icon: faSoundcloud,
+        url: "https://soundcloud.com/capricatalogue"
+    },
+]
+
 function Header() {
     return(
         <nav className="navbar">
-        <h1>CAPRI</h1>
+
+        <a href="/"><p id="title">CAPRI</p></a>
+
+
+        <nav className="pages">
+            <ul>
+                <li><a href="#hero">Current Issue</a></li>
+                <li><a href="#archive">Archive</a></li>
+                <li><a href="#donate">Donate</a></li>
+            </ul>
+        </nav>
+
+
+        {<nav className="icons">
+                <a href={socials[0].url}>
+                    <FontAwesomeIcon icon={socials[0].icon} size="1x" />
+                </a>
+                <a href={socials[1].url}>
+                    <FontAwesomeIcon icon={socials[1].icon} size="1x" />
+                </a>
+                <a href={socials[2].url}>
+                    <FontAwesomeIcon icon={socials[2].icon} size="1x" />
+                </a>
+                <a href={socials[3].url}>
+                    <FontAwesomeIcon icon={socials[3].icon} size="1x" />
+                </a>
+                <a href={socials[4].url}>
+                    <FontAwesomeIcon icon={socials[4].icon} size="1x" />
+                </a>
+                <a href={socials[5].url}>
+                    <FontAwesomeIcon icon={socials[5].icon} size="1x" />
+                </a>
+        </nav>}
+
+        <a href="#contact">
+        <button className="navButton">
+            Get In Touch
+        </button>
+        </a>
         </nav>
     )
 }
