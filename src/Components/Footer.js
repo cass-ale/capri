5b5bf3e7e622477198b57eb2058db08eea775159
Cassian Aleria <111076541+cass-ale@users.noreply.github.com>
2023-04-01 19:22:03 -0400

    Finished styling footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,10 +1,44 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import SubForm from "./SubForm";
 
+const handleClick = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  };
+const handleMouseOver = () => {
+        const text = document.querySelector('#capri');
+        text.innerHTML = '`kæpri';
+}
+const handleMouseOut = () => {
+        const text = document.querySelector('#capri');
+        text.innerHTML = 'CAPRI';
+}
 function Footer() {
     return (
-        <div>
-        <p>© CAPRI Media {new Date().getFullYear().toString()}. All Rights Reserved. Use of this site constitutes acceptance of our <Link to="/privacy">Privacy</Link> and <Link to="/cookies">Cookie</Link> policies. The material on this site may not be reproduced, distributed, transmitted, cached or otherwise used, except with the prior written permission of CAPRI Media.</p>
+        <div className="footerContainer">
+
+
+        <div className="footerTop">
+
+        {/* Logo that doubles as a back to top scroll button */}
+        <section id="capri" onClick={handleClick} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>CAPRI</section>
+
+        {/* Email Submission Form */}
+        <SubForm />
+
+        <div className="footerLinks">
+            <Link to='/Donate'>DONATE</Link>
+            <Link to='/Transparency'>TRANSPARENCY</Link>
+        </div>
+
+
+        </div>
+
+        <hr />
+        <section id='copyright'>© CAPRI Media {new Date().getFullYear().toString()}. All Rights Reserved. Use of this site constitutes acceptance of our <Link to="/privacy">Privacy</Link> and <Link to="/cookies">Cookie</Link> policies. The material on this site may not be reproduced, distributed, transmitted, cached or otherwise used, except with the prior written permission of CAPRI Media.</section>
         </div>
     )
 }
--- src/Components/SubForm.js
+++ src/Components/SubForm.js
@@ -1,10 +1,41 @@
 import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 function SubForm() {
     return(
-        <div>
-            
-        </div>
+        <>
+        {/* Email Submission Form */}
+        <Formik
+        initialValues={{ email: "" }}
+        validationSchema={Yup.object({
+          email: Yup.string()
+            .email("Invalid Email Address")
+            .required(""),
+        })}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <div className="footerForm">
+            <label htmlFor="email">Subscribe To The CAPRI Catalogue For Occasional Updates From Us.</label>
+            <section id="footerForm">
+            <Field type="email" name="email" />
+            <ErrorMessage name="email" />
+            <button className='footerButton' type="submit" disabled={isSubmitting}>
+              Keep In Touch
+            </button>
+            </section>
+            </div>
+          </Form>
+        )}
+      </Formik>
+        </>
     )
 }
 
