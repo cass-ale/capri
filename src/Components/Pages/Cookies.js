6cb45cfcefe142be951c0c65f4fad5c393789592
Cassian Aleria <111076541+cass-ale@users.noreply.github.com>
2023-04-02 22:48:00 -0400

    Policy Pages formatted

--- src/Components/Pages/Cookies.js
+++ src/Components/Pages/Cookies.js
@@ -14,53 +14,50 @@ function Cookies() {
         </header>
 
 
-        <main>
-            For more information about how we may use personal data, please read our privacy policy or email admin@caprimag.com.
+        <main className="privacy">
+            <div className='privacyContainer'>
+            <p>For more information about how we may use personal data, please read our <Link to='/privacy'>privacy policy</Link> or email <Link to='mailto:admin@caprimag.com'>admin@caprimag.com</Link>.</p>
 
-Cookie Policy
-Last Updated: 03 June 2022.
+<p id="h1">Cookie Policy</p>
+<i>Last Updated: 1 April 2023.</i>
 
-What Are Cookies
+<h4>What Are Cookies</h4>
 
 A cookie is a small file that can be placed on your device. It is sent to your browser and stored on your computer’s hard drive or tablet or mobile device. When you visit our sites or apps it can allow us to recognise and remember you. This page describes what information they gather, how we use it and why we sometimes need to store these cookies. We will also share how you can prevent these cookies from being stored, however this may downgrade certain elements of the site’s functionality.
 
-How We Use Cookies
+<h4>How We Use Cookies</h4>
 
 We use cookies for a variety of reasons detailed below. Unfortunately in most cases there are no industry standard options for disabling cookies without completely disabling the functionality and features they add to this site. It is recommended that you leave on all cookies if you are not sure whether you need them or not in case they are used to provide a service that you use.
 
-First-party and third-party cookies
+<h4>First-party and third-party cookies</h4>
 
 There are different types of cookies:
-
-First-party cookies – cookies that are set by Capri when you use our site
-
-Third-party cookies – cookies that are set by an organisation other than Capri when you use our site. Some Capri web pages may also contain content from other sites which may set their own cookies. If you share a link to a Capri page, the service you share it on may set a cookie on your browser.
-
-How long do cookies last?
-
-Session cookies – cookies that only last as long as your online session, and expire when you close your browser such as Internet Explorer or Safari
-
-Persistent cookies – cookies that stay on your device after your browser has been closed and last for a time specified in the cookie (but not longer than 13 months). We use these cookies when we need to remember you for more than one browsing session, for instance to remember your preferences from one visit to the next.
-
-What types of cookies do we use?
+<ul>
+<li><i>First-party cookies</i> – cookies that are set by CAPRI when you use our site.</li>
+<li><i>Third-party cookies</i> – cookies that are set by an organisation other than CAPRI when you use our site. Some CAPRI web pages may also contain content from other sites which may set their own cookies. If you share a link to a CAPRI page, the service you share it on may set a cookie on your browser.</li>
+</ul>
+<h4>How long do cookies last?</h4>
+<ul>
+<li><i>Session cookies</i> – cookies that only last as long as your online session, and expire when you close your browser such as Internet Explorer or Safari.</li>
+<li><i>Persistent cookies</i> – cookies that stay on your device after your browser has been closed and last for a time specified in the cookie (but not longer than 13 months). We use these cookies when we need to remember you for more than one browsing session, for instance to remember your preferences from one visit to the next.</li>
+</ul>
+<h4>What types of cookies do we use?</h4>
 
 We use three types of cookies:
-
-Performance - cookies which measure how often you visit our sites and how you use them. We use this information to get a better sense of how our users engage with our journalism and to improve our sites and apps, so that users have a better experience. For example, we collect information about which of our pages are most frequently visited, and by which types of users. We also use third-party cookies to help with performance.
-
-Functionality – cookies that are used to recognise you and remember your preferences or settings when you return to our site, so that we can provide you with a more personalised experience. For example, if you are based in the United Kingdom, we will remember this and make sure that you receive the UK homepage when you visit our site, rather than the US or Australia homepage. A mix of first-party and third-party cookies are used.
-
-Advertising – cookies that are used to collect information about your visit to our site, including the content you have viewed, the links you have followed and information about your browser, device and your IP address. We have set out more details on this below.
-
-How do we use cookies for advertising?
+<ul>
+<li><i>Performance</i> - cookies which measure how often you visit our sites and how you use them. We use this information to get a better sense of how our users engage with our journalism and to improve our sites and apps, so that users have a better experience. For example, we collect information about which of our pages are most frequently visited, and by which types of users. We also use third-party cookies to help with performance.</li>
+<li><i>Functionality</i> – cookies that are used to recognise you and remember your preferences or settings when you return to our site, so that we can provide you with a more personalised experience. For example, if you are based in the United Kingdom, we will remember this and make sure that you receive the UK homepage when you visit our site, rather than the US or Australia homepage. A mix of first-party and third-party cookies are used.</li>
+<li><i>Advertising</i> – cookies that are used to collect information about your visit to our site, including the content you have viewed, the links you have followed and information about your browser, device and your IP address. We have set out more details on this below.</li>
+</ul>
+<h4>How do we use cookies for advertising?</h4>
 
 As you browse our site, some of the cookies and similar technology we place on your device are for advertising, so we can understand what sorts of pages you read and are interested in. We can then display advertising on your browser based on these interests. For instance, if you have been reading a lot of food and drink articles, you may be shown more adverts for food and drink from others.
 
 Our apps integrate third-party software that provides us with information about how the app is used and what content you have viewed for the purposes of online advertising and analytics, in a similar way to our sites. It uses cookies from some of the providers listed in the Privacy settings, that can be found in the footer of every page on our site. The advertising techniques we use do not collect personal data such as your name, email address, postal address or phone number. We sometimes use information such as your IP address and browser type and also sometimes share some limited aspects of this with others for advertising purposes.
 
-We also allow other organisations to use cookies and other technology to help us analyse how our site is being used, measure the number of visitors to the site, and display advertising. To accomplish these goals, Capri utilises cookies, tags, pixels, and other web beacons to communicate information about you to others. While some of this information is only used on behalf of Capri, we also allow certain selected third parties to use cookies for advertising.
+We also allow other organisations to use cookies and other technology to help us analyse how our site is being used, measure the number of visitors to the site, and display advertising. To accomplish these goals, CAPRI utilises cookies, tags, pixels, and other web beacons to communicate information about you to others. While some of this information is only used on behalf of CAPRI, we also allow certain selected third parties to use cookies for advertising.
 
-How do third parties use cookies for advertising?
+<h4>How do third parties use cookies for advertising?</h4>
 
 We share and receive online data collected through cookies and similar technology with our advertising partners. This means that when you are on another website, you may be shown advertising based on your browsing patterns on our sites. We may also show you advertising on our sites based on your browsing patterns on other sites that we have obtained from our advertising partners.
 
@@ -68,57 +65,53 @@ Online retargeting is another form of online advertising that allows us and some
 
 Other organisations also collect user information on our sites through cookies, tags and pixels. Tags and pixels, also known as web beacons, are similar to cookies but are collected through embedded images.
 
-The Cookies We Set
+<h4>The Cookies We Set</h4>
+<ul>
+<li>Email newsletters related cookies</li>
 
-Email newsletters related cookies
+This site offers newsletter or email subscription services and cookies may be used to remember if you are already registered and whether to show certain notifications which might only be valid to subscribed/unsubscribed users.<br /><br />
 
-This site offers newsletter or email subscription services and cookies may be used to remember if you are already registered and whether to show certain notifications which might only be valid to subscribed/unsubscribed users.
+<li>Forms related cookies</li>
 
-Forms related cookies
+When you submit data to through a form such as those found on contact pages or comment forms cookies may be set to remember your user details for future correspondence.<br /><br />
 
-When you submit data to through a form such as those found on contact pages or comment forms cookies may be set to remember your user details for future correspondence.
-
-Site preferences cookies
+<li>Site preferences cookies</li>
 
 In order to provide you with a great experience on this site we provide the functionality to set your preferences for how this site runs when you use it. In order to remember your preferences we need to set cookies so that this information can be called whenever you interact with a page is affected by your preferences.
-
-Third Party Cookies
+</ul>
+<h4>Third Party Cookies</h4>
 
 In some special cases we also use cookies provided by trusted third parties. The following section details which third party cookies you might encounter through this site.
+<ul>
+<li>This site uses Google Analytics which is one of the most widespread and trusted analytics solution on the web for helping us to understand how you use the site and ways that we can improve your experience. These cookies may track things such as how long you spend on the site and the pages that you visit so we can continue to produce engaging content.</li><br />
 
-This site uses Google Analytics which is one of the most widespread and trusted analytics solution on the web for helping us to understand how you use the site and ways that we can improve your experience. These cookies may track things such as how long you spend on the site and the pages that you visit so we can continue to produce engaging content.
+For more information on Google Analytics cookies, see the official <Link to='https://support.google.com/analytics?hl=en&utm_campaign=marketingplatform.google.com%2Fabout%2Fsupport%2F&utm_medium=et&utm_source=marketingplatform.google.com#topic=10737980'>Google Analytics page</Link>.<br /><br />
 
-For more information on Google Analytics cookies, see the official Google Analytics page.
-
-From time to time we test new features and make subtle changes to the way that the site is delivered. When we are still testing new features these cookies may be used to ensure that you receive a consistent experience whilst on the site whilst ensuring we understand which optimisations our users appreciate the most.
-
-How to manage cookies
+<li>From time to time we test new features and make subtle changes to the way that the site is delivered. When we are still testing new features these cookies may be used to ensure that you receive a consistent experience whilst on the site whilst ensuring we understand which optimisations our users appreciate the most.</li>
+</ul>
+<h4>How to manage cookies</h4>
 
 You are able to decline our use of cookies by clicking the “Decline” button located in the pop-up at the bottom right of every page.
 
-You can switch some cookies off through the Your Online Choices site. You may need to do this again each time you use a different IP address, device, or virtual private network. You can also adjust the global privacy setting or plug-in which should be communicated to use via your browser.
+You can switch some cookies off through the <Link to='http://www.youronlinechoices.com/uk'>Your Online Choices site</Link>. You may need to do this again each time you use a different IP address, device, or virtual private network. You can also adjust the global privacy setting or plug-in which should be communicated to use via your browser.
 
 It is also possible to stop your browser from accepting cookies altogether by changing your browser’s cookie settings. You can usually find these settings in the “options” or “preferences” menu of your browser. The following links may be helpful, or you can use the “Help” option in your browser. If you do this some features on our sites may not work as well as you expect.
-
-Cookie settings in Internet Explorer
-
-Cookie settings in Firefox
-
-Cookie settings in Chrome
-
-Cookie settings in Safari web and iOS
-
-Useful links
+<ul>
+<li><Link to='http://windows.microsoft.com/en-GB/internet-explorer/delete-manage-cookies#ie=ie-10'>Cookie settings in Internet Explorer</Link></li>
+<li><Link to='http://support.mozilla.com/en-US/kb/Cookies'>Cookie settings in Firefox</Link></li>
+<li><Link to='https://support.google.com/chrome/answer/95647?hl=en&ref_topic=14666'>Cookie settings in Chrome</Link></li>
+<li><Link to='https://support.apple.com/kb/PH17191?locale=en_US'>Cookie settings in Safari web</Link> and <Link to='http://support.apple.com/kb/HT1677'>iOS</Link></li>
+</ul>
+<h4>Useful links</h4>
 
 If you would like to find out more about privacy, cookies and their use on the internet, you may find the following links useful:
-
-Microsoft cookies guide
-
-All About Cookies
-
-The Information Commissioner’s Office
-
-If you would like to contact us about cookies please email admin@caprimag.com.
+<ul>
+<li><Link to='https://support.microsoft.com/en-us/help/260971/description-of-cookies'>Microsoft cookies guide</Link></li>
+<li><Link to='http://www.allaboutcookies.org/'>All About Cookies</Link></li>
+<li><Link to='https://ico.org.uk/'>The Information Commissioner’s Office</Link></li>
+</ul>
+If you would like to contact us about cookies please email <Link to='mailto:admin@caprimag.com'>admin@caprimag.com</Link>.
+</div>
             </main>
 
             <footer>
