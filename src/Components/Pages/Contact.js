7f765f056b479e69a47d0febd6409b601d6fe4ad
Cass Aleria <111076541+cass-ale@users.noreply.github.com>
2023-04-06 14:17:12 -0400

    UX Boosts; scroll functionality added to contact form

--- src/Components/Pages/Contact.js
+++ src/Components/Pages/Contact.js
@@ -1,16 +1,25 @@
-import React, {useState} from "react";
+import React, {useState, useRef} from "react";
 import Header from "../Header";
 import Footer from "../Footer";
 import { Helmet } from "react-helmet";
 import Form from '../ContactForm';
 
 function Contact() {
+    const targetRef = useRef(null);
     const [showForm, setShowForm] = useState(false);
     const openForm = () => {
         setShowForm(true);
+        targetRef.current.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+          });
     };
     const closeForm = () => {
         setShowForm(false);
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+          });
     }
     return(
         <>
@@ -24,7 +33,7 @@ function Contact() {
         <main>
         <div className="contactContainer">
 
-            <section id="conHeader">Get In Touch With CAPRI</section>
+            <section id="conHeader" ref={targetRef}>Get In Touch With CAPRI</section>
 
             <p>If You Are Interested In Our Work, Interested In Working With Us, Or Eager To Talk With Us About Our Work, Feel Free To Fill Out Our Contact Form, And Our Team Will Be In Touch With You Shortly After.<br /><br /> Please Note That Our Administrative Team Only Monitors Our Inbox During U.S. Business Hours (9:00-17:00) On Monday Through Friday. We Look Forward To Hearing From You!</p>
 
