a2917fdde9f6625231cffca784cfa97decb76f8e
Cass Aleria <111076541+cass-ale@users.noreply.github.com>
2023-04-06 22:48:45 -0400

    Cleaning

--- src/Components/MobileMenu.js
+++ src/Components/MobileMenu.js
@@ -1,5 +1,4 @@
 import React, { useEffect} from "react";
-import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClose } from "@fortawesome/free-solid-svg-icons";
 
