b132f393b9cbf40fa20aff576fc8cd15195c12d5
Jace Arouet <jarouet@fb.com>
2023-04-21 11:21:01 -0400

    Carousel done, added buttons; cleaned up CSS file

--- src/Components/Pages/CASS/Cass.js
+++ src/Components/Pages/CASS/Cass.js
@@ -2,7 +2,6 @@ import React, {useState, useEffect} from 'react'
 import '../../../cass.modules.css';
 import Header from './Cheader';
 import Footer from './Cfooter';
-import Cprojects from './Cprojects';
 import Csites from './Csites';
 import PopUp from './Cpopup';
 import { Helmet } from 'react-helmet';
@@ -13,7 +12,6 @@ import { Helmet } from 'react-helmet';
 function Cass() {
   const [showPopUp, setShowPopUp] = useState(true);
     const showPopupHandler = () => setShowPopUp(false);
-
     const reopen = () => setShowPopUp(true);
 
 
@@ -55,11 +53,13 @@ function Cass() {
 
 
             <h2>Front-End Projects</h2>
-            <p id='about'>After Receiving My Front-End Developer Certification Through Metas Coursera Learning Program In March Of 2023, I Immediately Began Working On Publishing Live Websites Made With React.js
+            <p id='about'>After Receiving My Front-End Developer Certification Through Meta's Coursera Learning Program In March Of 2023, I Immediately Began Working On Publishing Live Websites Made With React.js
             To Solidify My Understanding Of Front End Development Principles And To Prepare Myself For The Tech Job Market. Below You Will Find Links To Full Sites I Have Created
             In React. These Projects Showcase My Understanding Of UX/Accessibility Design, Responsive Applications, And Organized Project Management. </p>
-<div className='caro'>
             <Csites />
+<div className='caro'>           
+            <a href="https://www.caprimag.com" target="_blank" rel="noopener noreferrer"><button><span id='cjacapri'>CAPRI Media</span></button></a>
+            <a href="https://www.simpcxty.com" target="_blank" rel="noopener noreferrer"><button><span id='simp'>simpcxty</span></button></a>
 </div>
 
             </main>
--- src/Components/Pages/CASS/Csites.js
+++ src/Components/Pages/CASS/Csites.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../../../cass.modules.css';
 import capri from '../../../Images/capri.png';
 import simp from '../../../Images/simp.png';
-import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import { CarouselProvider, Slider, Slide } from 'pure-react-carousel';
 import 'pure-react-carousel/dist/react-carousel.es.css';
 
 
@@ -11,15 +11,27 @@ function Csites() {
         <>
         <CarouselProvider
         naturalSlideWidth={999}
-        naturalSlideHeight={650}
+        naturalSlideHeight={775}
         totalSlides={2}
         interval={10000}
         isPlaying={true}
         dragEnabled={true}
-        touchEnabled={true}>
+        touchEnabled={true}
+        lockOnWindowScroll={true}
+        infinite={true}>
         <Slider>
-        <Slide index={0}><a href="https://www.caprimag.com" target="_blank" rel="noopener noreferrer"><img src={capri} className='img' alt='The Official Website For CAPRI Media Entertainment. Coded From Scratch Using React.Js.' /></a></Slide>
-        <Slide index={1}><a href="https://www.simpcxty.com" target="_blank" rel="noopener noreferrer"><img src={simp} className='img' alt='The Official Website For Up And Coming Hip-Hop And R&B Artist, simpcxty. Coded From Scratch Using React.Js.' /></a></Slide>
+        <Slide index={0}>
+            <img src={capri} className='img' alt='The Official Website For CAPRI Media Entertainment. Coded From Scratch Using React.Js.' />
+            <p>The Official Website For CAPRI Media Entertainment. Coded From Scratch Using React.Js, This Website Is Able To Smoothly Transition Between Devices To Present A Modern And Responsive Experience To Users.
+                 CAPRI Media Is A Fashion And Art Production Company That Required A Website That Could Be As Sleek And Timeless As Their Works.
+                 Github Repository For The Source Code Is Private At The Moment. Please Reach Out To View The Code Via Email.</p>
+            </Slide>
+        <Slide index={1}>
+            <img src={simp} className='img' alt='The Official Website For Up And Coming Hip-Hop And R&B Artist, simpcxty. Coded From Scratch Using React.Js.' />
+            <p>The Official Website For Up And Coming Hip-Hop/R&B Artist, simpcxty. Born And Raised In Bessemer, AL, simpcxty Aims To Challenge Outdated Notions Of Masculinity And 
+                Encourage Men To Embrace Their Emotions In The Name Of Bettering Mental Health. This Site Is Coded In React.Js And Is Easily Updated To Suit The Changing Styles Of The Musician; 
+                The Site Is Also High-Performing And Responsive To Capture The Eyes Of Potential Fans Whether They Be On Desktop Or Mobile. Github Repository For The Source Code Is Private At The Moment. Please Reach Out To View The Code Via Email. </p>
+            </Slide>
         </Slider>
         </CarouselProvider>
         </>
