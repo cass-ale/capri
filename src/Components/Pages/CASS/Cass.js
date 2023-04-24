030d3a37e3db42dcebd80eeb7804d051c65b0038
Cassian Aleria <111076541+cass-ale@users.noreply.github.com>
2023-04-23 21:21:23 -0400

    Mobilized and optimized portfolio!

--- src/Components/Pages/CASS/Cass.js
+++ src/Components/Pages/CASS/Cass.js
@@ -7,6 +7,8 @@ import PopUp from './Cpopup';
 import { Helmet } from 'react-helmet';
 import AudioPlayer from 'react-h5-audio-player';
 import songs from './Songs';
+import { Tooltip as ReactTooltip } from 'react-tooltip';
+import 'react-tooltip/dist/react-tooltip.css';
 
 
 
@@ -74,15 +76,23 @@ const handleBack = () => {
 
 
 
-            <h2>Front-End Projects</h2>
+            <h2>About Me & My Work</h2>
             <p id='about'>After Receiving My <a href="https://www.coursera.org/account/accomplishments/specialization/certificate/Q3TW5TCE2NN7" target="_blank" rel="noopener noreferrer">Front-End Developer Certification</a> Through Meta's Coursera Learning Program In March Of 2023, I Immediately Began Working On Publishing Live Websites Made With React.js
             To Solidify My Understanding Of Front End Development Principles And To Prepare Myself For The Tech Job Market. Below You Will Find Links To Full Sites I Have Created
             In React. These Projects Showcase My Understanding Of UX/Accessibility Design, Responsive Applications, And Organized Project Management. </p>
+
+
+
+            <h2 id="prev">Previews Of Websites I've Made In React</h2>
+            <p id='click'>Click And Drag To The Left To Cycle Through!</p>
+            <p id='mobileClick'>Swipe To Cycle Through!</p>
             <Csites />
-          <div className='caro'>
-            <a href="https://www.caprimag.com" target="_blank" rel="noopener noreferrer"><button><span id='cjacapri'>CAPRI Media</span></button></a>
-            <a href="https://www.simpcxty.com" target="_blank" rel="noopener noreferrer"><button><span id='simp'>simpcxty</span></button></a>
-          </div>
+            <div className='caro'>
+                <a href="https://www.caprimag.com" target="_blank" rel="noopener noreferrer"><button data-tooltip-id='capbutt' data-tooltip-content="I'll Take You To The CAPRI Media Website!"><span id='cjacapri'>CAPRI Media</span></button></a>
+                <ReactTooltip id="capbutt" />
+                <a href="https://www.simpcxty.com" target="_blank" rel="noopener noreferrer"><button data-tooltip-id='simpbutt' data-tooltip-content="I'll Take You To The simpcxty Website!"><span id='simp'>simpcxty</span></button></a>
+                <ReactTooltip id="simpbutt" />
+            </div>
 
         </main>
 
