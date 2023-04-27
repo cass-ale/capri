98cee250c1505c1320d8acdf6a5fffc143558e90
Cassian Aleria <111076541+cass-ale@users.noreply.github.com>
2023-04-26 23:17:37 -0400

    refix?

--- config/webpack/persistentCache/createEnvironmentHash.js
+++ config/webpack/persistentCache/createEnvironmentHash.js
@@ -1,7 +1,8 @@
-
+'use strict';
+const { createHash } = require('crypto');
 
 module.exports = env => {
-  const hash = crypto.createHash('sha512WithRSAEncryption');
+  const hash = createHash('md5');
   hash.update(JSON.stringify(env));
 
   return hash.digest('hex');
