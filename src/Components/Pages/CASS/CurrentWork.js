b66d26dc896cb82941c7b01661b72ee843e33ff0
Jace Arouet <jarouet@fb.com>
2023-06-02 16:38:51 -0400

    Styling

--- src/Components/Pages/CASS/CurrentWork.js
+++ src/Components/Pages/CASS/CurrentWork.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Button } from '@mui/material';
 
 
 
@@ -8,7 +8,24 @@ function CurrentWork() {
         <>
         <div className='currentWork'>
             <section className='capriMedia'>
-            <h1>CAPRI Media</h1>
+            <h1>Project One: CAPRI Media</h1>
+            <p>CAPRI Media is an NYC Based Fashion & Photography collective founded in 2022. In this project I built and designed their official website's UI in React.js using modern approaches to responsive and accessible design. Read further into the User Experience case study to see how I iterated on the UI to imrpove the experience for CAPRI's users.</p>
+            <Button
+            sx={{
+            fontFamily: "Gopher",
+            fontSize: "1rem",
+            color: "white",
+        }}>CAPRI Media UX Case Study</Button>
+            </section>
+            <section className='simpcxty'>
+            <h1>Project Two: simpxty</h1>
+            <p>simpcxty is an aspiring R&B Singer/Songwrtier from Bessemer, AL, and we collaborated to build his official website to consolidate his works to create a hub for fans to easily access more of the original content they love and also allows for potential business connections to reach out</p>
+            <Button
+            sx={{
+            fontFamily: "Gopher",
+            fontSize: "1rem",
+            color: "white",
+        }}>simpcxty</Button>
             </section>
         </div>
         </>
--- src/Components/Pages/Donate.js
+++ src/Components/Pages/Donate.js
@@ -2,19 +2,15 @@ import React from "react";
 import Header from "../Header";
 import Footer from "../Footer";
 import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPaypal } from '@fortawesome/free-brands-svg-icons';
-import { faMoneyBills } from "@fortawesome/free-solid-svg-icons";
 import { Helmet } from "react-helmet";
 import ScrollToTopOnMount from "../Scroll";
+import { Button } from '@mui/material';
 
 const dono = [
     {
-        icon: faPaypal,
         url: 'https://www.paypal.com/paypalme/capricatalogue'
     },
     {
-        icon: faMoneyBills,
         url: 'https://account.venmo.com/u/capricatalogue',
     }
 ]
@@ -43,28 +39,38 @@ function Donate() {
         <div className="donoContainer">
 
         <p id="h1" style={{marginTop: "2rem"}}>Support For CAPRI Media</p>
-
         <p>
             CAPRI Media would like our values and ethics to be apparent from the very beginning. Therefore, a portion of all revenue earned through CAPRI related projects (50% of all direct donations) will be donated to both <Link to='https://nomore.org/take-action/' target="_blank" rel="noopener noreferrer"><b>NO MORE</b></Link> and <Link to='https://www.amirahinc.org/our-impact' target="_blank" rel="noopener noreferrer"><b>Amirah, Inc.</b></Link> Donating to CAPRI helps us to fund our creative endeavors and to properly compensate all of our collaborators for their invaluable time and effort.<br/><br />
 
-            We hope that our allyship inspires you to support, believe, and celebrate women and other survivors of sexual violence. <br /><br/>
-
+            Please find resources below provided by RAINN (Rape, Abuse & Incest National Network), the nation's largest anti-sexual violence organization. We think the ability to support loved ones who have experienced sexual violence is crucial, and thanks to RAINN, CAPRI is able to share the ability to TALK<sup>1</sup> with family and friends.<br/>
+            <blockquote>Whether someone you love has disclosed to you already, or you just want to be prepared for the moment someone does, taking the time to proactively learn how to support a survivor as they disclose can make all the difference.</blockquote>
+            We hope that our allyship inspires you to support, believe, and celebrate women and other survivors of sexual violence.
             You can find links for our PayPal and Venmo below, and thank you for supporting CAPRI Media.</p> <br /><br/>
 
             <section id="sig" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>— CAPRI</section>
             <section id="mobileSig">— CAPRI</section>
         <br />
-
-        {<nav className="mobileIcons">
+        </div>
+        <section className="donoButtons">
                 <a href={dono[0].url} target="_blank" rel="noopener noreferrer">
-                    <FontAwesomeIcon icon={dono[0].icon} size="3x" />
+                    <Button
+                    sx={{
+                        fontFamily: "Gopher",
+                        fontSize: "2rem",
+                        color: "black",
+                    }}> PayPal </Button>
                 </a>
                 <a href={dono[1].url} target="_blank" rel="noopener noreferrer">
-                    <FontAwesomeIcon icon={dono[1].icon} size="3x" />
+                    <Button
+                    sx={{
+                        fontFamily: "Gopher",
+                        fontSize: "2rem",
+                        color: "black",
+                    }}> Venmo </Button>
                 </a>
-        </nav>}<br />
-
-        </div>
+        </section>
+        <a href="https://ohl.rainn.org/online/"><img className="rainn" src="https://rainn.org/files/uploadedFiles/OVC_SM_ENG.jpg" alt="National Sexual Assault Hotline Banner" /></a>
+        <p className="citation"><br /><br /><br /><br />1. <i>How to support a loved one</i>. RAINN. (n.d.). <a href="https://rainn.org/TALK">https://rainn.org/TALK</a> </p>
         </main>
 
         <footer>
