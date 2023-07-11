e2780a8e7bcc6f7c775346576b945eca6e252310
Jace Arouet <jarouet@fb.com>
2023-07-11 15:13:11 -0400

    restyle

--- src/Components/Pages/CASS/Coda.js
+++ src/Components/Pages/CASS/Coda.js
@@ -15,11 +15,18 @@ const Coda = () => {
     <h1>CODA</h1>
     <h2>Connect & Reconnect</h2>
     <p>CODA is a Discord-style messaging app that lets users message their friends anytime and anywhere. The Frontend UI is developed using Vite, and I am using Firebase to handle the Backend data storage. Not only was this a fun project to get to work on, it is a fun and cute web app that offers a nostalgic chatroom for users to catch up with their close friends!</p>
+    <article>
     <a href='https://codamessaging.netlify.app/' target="_blank" rel="noopener noreferrer"><Button size='large' sx={{
         color: "#D90754",
         fontFamily: "Gopher",
         fontSize: "2rem"
       }}>Try CODA!</Button></a>
+    <a href='https://docs.google.com/document/d/1G6siiypxBhOQj-FTe_qMaGBId36iSwWFDbaxV25hn5Q/edit?usp=sharing' target="_blank" rel="noopener noreferrer"><Button size='large' sx={{
+        color: "#D90754",
+        fontFamily: "Gopher",
+        fontSize: "2rem"
+      }}>CODA UX Study</Button></a>
+    </article>
     </section>
       {/*<img src='' alt='' />*/}
     </div>
--- src/Components/Pages/CASS/CurrentWork.js
+++ src/Components/Pages/CASS/CurrentWork.js
@@ -9,7 +9,7 @@ function CurrentWork() {
         <div className='currentWork' id='current'>
             <section className='capriMedia'>
             <h1>Project One: CAPRI Media</h1>
-            <p>CAPRI Media is an NYC Based Fashion & Photography collective founded in 2022. In this project I built and designed their official website's UI in React.js using modern approaches to responsive and accessible design. Read further into the User Experience case study to see how I iterated on the UI to improve the experience for CAPRI's users.</p>
+            <p>CAPRI Media is an NYC Based Fashion & Photography collective founded in 2022. In this project I built and designed their official website's UI in React.js using modern approaches to responsive and accessible design.</p>
             <section className='cardButtons'>
             <a href='https://www.caprimag.com' target="_blank" rel="noopener noreferrer"><Button
             sx={{
@@ -17,12 +17,6 @@ function CurrentWork() {
             fontSize: "1rem",
             color: "white",
         }}>CAPRI Media</Button></a>
-           <a href='https://docs.google.com/document/d/1mE3cumzPsqyhpoR5tlQkUTUKuhaHeaNWIKebIwS2kl4/edit' target="_blank" rel="noopener noreferrer"><Button
-            sx={{
-            fontFamily: "Gopher",
-            fontSize: "1rem",
-            color: "white",
-        }}>CAPRI Media UX Case Study</Button></a>
         </section>
             </section>
             <section className='simpcxty'>
@@ -35,12 +29,6 @@ function CurrentWork() {
             fontSize: "1rem",
             color: "white",
         }}>simpcxty</Button></a>
-       <a href='https://docs.google.com/document/d/1G6siiypxBhOQj-FTe_qMaGBId36iSwWFDbaxV25hn5Q/edit' target="_blank" rel="noopener noreferrer"><Button
-            sx={{
-            fontFamily: "Gopher",
-            fontSize: "1rem",
-            color: "white",
-        }}>simpcxty UX Case Study</Button></a>
         </section>
             </section>
         </div>
