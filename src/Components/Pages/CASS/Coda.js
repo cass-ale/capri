d4e5ddc71baf597f08f01a9de4e81b63c6978446
Jace Arouet <jarouet@fb.com>
2023-07-12 00:21:26 -0400

    codaaaaa

--- src/Components/Pages/CASS/Coda.js
+++ src/Components/Pages/CASS/Coda.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Button } from '@mui/material';
+import chats from "../../../Images/chats.png";
+import lap from "../../../Images/laptop.png";
+import coda from "../../../Images/CODA.png";
 
 
 
@@ -13,9 +16,18 @@ const Coda = () => {
     <div className='coda'>
     <section>
     <h1>CODA</h1>
+
+    <img src={chats} alt='' id='chats' />
+
     <h2>Connect & Reconnect</h2>
-    <p>CODA is a Discord-style messaging app that lets users message their friends anytime and anywhere. The Frontend UI is developed using Vite, and I am using Firebase to handle the Backend data storage. Not only was this a fun project to get to work on, it is a fun and cute web app that offers a nostalgic chatroom for users to catch up with their close friends!
-    <br /><br />Click below to read the UX Case Study for CODA!</p>
+
+    <img src={lap} alt='' id='laptop' />
+
+    <p>CODA is a Discord-style messaging app that lets users message their friends anytime and anywhere. The Frontend UI is developed using Vite, and I am using Firebase to handle the Backend data storage. Not only was this a fun project to get to work on, it is a fun and cute web app that offers a nostalgic chatroom for users to catch up with their close friends!</p>
+
+    <div className='phones' />
+
+    <p><br /><br />Click below to read the UX Case Study for CODA!</p>
     <article>
     <a href='https://codamessaging.netlify.app/register' target="_blank" rel="noopener noreferrer"><Button size='large' sx={{
         color: "#D90754",
@@ -28,8 +40,9 @@ const Coda = () => {
         fontSize: "2rem"
       }}>CODA UX Study</Button></a>
     </article>
+
+      <img src={coda} alt='' id='logo' />
     </section>
-      {/*<img src='' alt='' />*/}
     </div>
   )
 }
