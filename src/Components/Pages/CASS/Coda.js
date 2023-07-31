14a287a3632d3fac6ff0c231d43857e835211c07
Cassian Aleria <111076541+cass-ale@users.noreply.github.com>
2023-07-31 17:40:02 -0400

    restyling

--- src/Components/Pages/CASS/Coda.js
+++ src/Components/Pages/CASS/Coda.js
@@ -3,7 +3,7 @@ import { Button } from '@mui/material';
 import chats from "../../../Images/chats.png";
 import lap from "../../../Images/laptop.png";
 import coda from "../../../Images/CODA.png";
-
+import phones from "../../../Images/phones.png";
 
 
 
@@ -17,15 +17,20 @@ const Coda = () => {
     <section>
     <h1>CODA</h1>
 
-    <img src={chats} alt='' id='chats' />
 
+    
+    <div className='mocks'>
+    <img src={chats} alt='' id='chats' />
+    <img src={lap} alt='' id='laptop' />
+    <img src={phones} alt='' id='phones' />
+    </div>
     <h2>Connect & Reconnect</h2>
 
-    <img src={lap} alt='' id='laptop' />
+
 
     <p>CODA is a Discord-style messaging app that lets users message their friends anytime and anywhere. The Frontend UI is developed using Vite, and I am using Firebase to handle the Backend data storage. Not only was this a fun project to get to work on, it is a fun and cute web app that offers a nostalgic chatroom for users to catch up with their close friends!</p>
 
-    <div className='phones' />
+
 
     <p><br /><br />Click below to read the UX Case Study for CODA!</p>
     <article>
